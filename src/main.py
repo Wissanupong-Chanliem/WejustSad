189a824d934739c2c872a444473f5241472cde0b
WithBinman <kasidatesangsawang@gmail.com>
2024-10-13 21:42:44 +0700

    Update GameOver Page

--- src/main.py
+++ src/main.py
@@ -124,7 +124,8 @@ class HangManPage(Page):
         self.guess.render(self.screen_ref)
     def update(self, event: Event):
         if event.type == pygame.MOUSEBUTTONDOWN:
-            # Classic Mode button is clicked
+            # Confirm now is go to GameOverPage
+            # MenuBotton is ConfirmButton
             if self.menu_button.button_rect.collidepoint(pygame.mouse.get_pos()):
                 self.redirect_to("GameOver")
         if event.type == pygame.KEYDOWN:
@@ -134,7 +135,7 @@ class HangManPage(Page):
                 self.guess.update_text("\""+self.current_key+"\"")
                 print(f"Current key: {self.current_key}")
 
-class GameOver(Page):
+class GameOverPage(Page):
     def __init__(self,screen:pygame.Surface,resources):
         Page.__init__(self,screen,resources)
         self.title_text = (
@@ -148,13 +149,18 @@ class GameOver(Page):
         )
         self.score = (
             Text(resources.fonts["Kanit-Bold-Regular-Size"],"Score",PUPE_CYAN)
-            .set_coordinate((self.screen_ref.get_width()-150,20))
+            .set_coordinate((50,self.screen_ref.get_height()-155))
         )
         self.current_key = ""
     def render(self):
         self.title_text.render(self.screen_ref)
         self.menu_button.render(self.screen_ref)
         self.score.render(self.screen_ref)
+    def update(self, event: Event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Classic Mode button is clicked
+            if self.menu_button.button_rect.collidepoint(pygame.mouse.get_pos()):
+                self.redirect_to("MainMenu")
 
 
 class Game():
@@ -167,7 +173,8 @@ class Game():
         self.pages:dict[str,Page] = {
             "MainMenu":MainMenuPage(self.screen,self.resources),
             "Topic":TopicPage(self.screen,self.resources),
-            "HangMan":HangManPage(self.screen,self.resources)
+            "HangMan":HangManPage(self.screen,self.resources),
+            "GameOver":GameOverPage(self.screen,self.resources)
         }
 
     def load_resource(self) -> Resource:
