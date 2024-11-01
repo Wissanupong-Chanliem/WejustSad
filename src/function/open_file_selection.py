8f2b60d380f57f4b6666e137381cc609ee735839
CyanKagami <boonsitapoopeko@gmail.com>
2024-11-01 21:36:46 +0700

    fix: it's work on my machine!! (fix path)

--- src/function/open_file_selection.py
+++ src/function/open_file_selection.py
@@ -4,16 +4,10 @@ import os
 
 def open_file_selection():
     file = fd.askopenfilename(initialdir="/", title="Select file", filetypes=[("Text Files","*.txt")])
-    # do some file copying magic
-    # print(file.readlines())
-    # target = 'import'
-    # dir_parts = list(os.path.split(file))
-    # target_dir = dir_parts[0] + target + '/' + dir_parts[1]
     if check_format(file):
         print("Error")
         return
-    shutil.copy2(file,"C:\\Users\\User\\Documents\\งาน\\WejustSad\\static\\wordlist" )
-    #file.close()
+    shutil.copy2(file,"\\static\\wordlist" )
 
 def check_format(file):
     txt = open(file,"r",encoding="utf-8")
@@ -24,4 +18,3 @@ def check_format(file):
             return 
     return 0
     
-open_file_selection()
