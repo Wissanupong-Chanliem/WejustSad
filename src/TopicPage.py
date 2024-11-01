b7c8bd5455c690407d57c5e3f9251ae1ac19d2a4
D37un <degun2548@gmail.com>
2024-11-01 21:50:36 +0700

    feat: Add CheckBox of Show word list

--- src/TopicPage.py
+++ src/TopicPage.py
@@ -35,13 +35,21 @@ class TopicPage(Page):
             Text(resources.fonts["Kanit-Regular"],"Word",self.resources.colors["black"])
             .set_coordinate((400,85))
         )
+        self.show_word_list = (
+            Text(resources.fonts["Kanit-Regular"],"Show Word List",self.resources.colors["black"])
+            .set_coordinate((840,85))
+        )
         self.box_chack = (
-            
+            pygame.transform.scale_by((self.resources.images["unchack"]),0.02)
         )
         self.selected_topic_text = (
             Text(resources.fonts["Kanit-Header-1"],"Select Topic",self.resources.colors["black"])
             .set_coordinate((100,120))
         )
+        self.box_rect = pygame.Rect(
+            810,87,*self.box_chack.get_size()
+        )
+        self.show_word = False
 
     def render(self):
         self.title_text.render(self.screen_ref)
@@ -50,13 +58,24 @@ class TopicPage(Page):
         self.add_wordlist_button.render(self.screen_ref)
         self.back_to_main_menu.render(self.screen_ref)
         self.word.render(self.screen_ref)
+        self.show_word_list.render(self.screen_ref)
         self.selected_topic_text.render(self.screen_ref)
+        if self.show_word == True:
+            pygame.draw.rect(self.screen_ref,self.resources.assets[self.resources.members[self.resources.current_member]]["normal-color"], pygame.Rect(810, 87, 25, 25), 0, 6)
+            pygame.draw.rect(self.screen_ref,(0,0,0), pygame.Rect(810, 87, 25, 25), 2, 6)
+        else:
+            pygame.draw.rect(self.screen_ref,(0,0,0), pygame.Rect(810, 87, 25, 25), 2, 6)
         pygame.draw.rect(self.screen_ref,self.resources.assets[self.resources.members[self.resources.current_member]]["normal-color"], pygame.Rect(400, 140, 580, 400), 2, 10)
 
     def update(self, event: Event):
         mouse_pos = pygame.mouse.get_pos()
         self.topic_selection.update(event)
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.box_rect.collidepoint(mouse_pos):
+                if self.show_word == False:
+                    self.show_word = True
+                else:
+                    self.show_word = False
             if self.start_button.button_rect.collidepoint(mouse_pos):
                 if self.topic_selection.get_selected():
                     word_list = list(random_word.random_word(read_word_list(f"static/wordlist/{self.topic_selection.get_selected()}.txt")).items())
