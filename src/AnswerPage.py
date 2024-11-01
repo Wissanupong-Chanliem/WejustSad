07f45f1abb579980e568eeea151fce620fb20661
win1011987 <kasidatesangsawang@gmail.com>
2024-11-01 19:55:48 +0700

    make game color relate to charactor color

--- src/AnswerPage.py
+++ src/AnswerPage.py
@@ -14,24 +14,24 @@ class AnswerPage(Page):
     def __init__(self,screen:pygame.Surface,resources:Resource,data:data_in):
         Page.__init__(self,screen,resources)
         self.title_text = (
-            Text(resources.fonts["Kanit-Word"],"ถูกต้องง!",self.resources.colors["pupe-cyan"])
+            Text(resources.fonts["Kanit-Word"],"ถูกต้องง!",self.resources.assets[self.resources.members[self.resources.current_member]]["normal-color"])
             .set_coordinate((self.screen_ref.get_rect().centerx,70),origin_center = True)
         )
         self.next_button = (
-            Button(300,80,self.resources.colors["pupe-cyan"],4)
-            .add_text(resources.fonts["Kanit-Header"],"Next",self.resources.colors["black"])
+            Button(300,80,self.resources.assets[self.resources.members[self.resources.current_member]]["normal-color"],4)
+            .add_text(resources.fonts["Kanit-Header"],"Next",self.resources.colors["white"])
             .set_coordinate((self.screen_ref.get_width()-200,640),origin_center=True)
         )
         self.score = (
-            Text(resources.fonts["Kanit-Word"],f"Score : {data["score"]}",self.resources.colors["pupe-cyan"])
+            Text(resources.fonts["Kanit-Word"],f"Score : {data["score"]}",self.resources.assets[self.resources.members[self.resources.current_member]]["normal-color"])
             .set_coordinate((50,self.screen_ref.get_height()-120))
         )
         self.word = (
-            Text(resources.fonts["Kanit-Word"],f"{data["word"][0]}",self.resources.colors["pupe-cyan"])
+            Text(resources.fonts["Kanit-Word"],f"{data["word"][0]}",self.resources.assets[self.resources.members[self.resources.current_member]]["normal-color"])
             .set_coordinate((self.screen_ref.get_rect().centerx,470),origin_center = True)
         )
         self.meaning = (
-            Text(resources.fonts["Kanit-Word"],f"{data["word"][1]}",self.resources.colors["pupe-cyan"])
+            Text(resources.fonts["Kanit-Word"],f"{data["word"][1]}",self.resources.assets[self.resources.members[self.resources.current_member]]["normal-color"])
             .set_coordinate((self.screen_ref.get_rect().centerx,545),origin_center = True)
         )
         self.happy_pic = pygame.transform.scale_by(self.resources.images["pupe-happy"],0.5)
